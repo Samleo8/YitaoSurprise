fd568ded7582a5f34b2b4919b69a7ba63be030f9
Samuel Leong <leowhiz@yahoo.com.sg>
2020-06-13 23:04:27 +0800

    Update scripts

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Probably wanna dynamically load everything?
 
+var pageWidth;
+
+function updatePageWidth(){
+    pageWidth = getWidth();
+
+    // Maybe have to update each individual container to size
+    // which is kinda sad
+    console.log(pageWidth)
+}
