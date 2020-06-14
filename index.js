6c1b493c3c7ee4a144671fab063ca0b2ecae9f65
Samuel Leong <leowhiz@yahoo.com.sg>
2020-06-14 13:23:27 +0800

    Update base according to tutorial

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 // Probably wanna dynamically load everything?
 
-var pageWidth;
 
-function updatePageWidth(){
-    pageWidth = getWidth();
+var slideIndex = 1;
+showSlides(slideIndex);
 
-    // Maybe have to update each individual container to size
-    // which is kinda sad
-    console.log(pageWidth)
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide-container");
+    
+    slideIndex = n % slides.length;
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
 }
